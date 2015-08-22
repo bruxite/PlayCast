9f1575f7535e72e5f82328820126748e172b26d6
Mike Bruxvoort <mbruxvoort@bethany.org>
2015-08-22 11:11:34 -0400

    setting up github

--- PlayCast/controllers/homeController.js
+++ PlayCast/controllers/homeController.js
@@ -9,6 +9,32 @@
                 res.render("index", { title: "Games | PlayCast", error: err, games: games});
             });
         });
+
+        //Game Page
+        app.get('/game/:id', function (req, res, next) {
+            var gameId = req.params.id;
+
+            data.getGame(gameId, function(err, game) {
+                res.render('game', {
+                    title: 'Game ' + game.opponent,
+                    "game": game
+                });
+            });
+
+        });
+
+        app.post('/play/add', function (req, res, next) {
+            console.log(req.body);
+            data.addPlay(req, function (err, game) {
+                res.render('game', {
+                    title: 'Game ' + game.opponent,
+                    "game": game
+                });
+            });
+
+        });
+
+
     };
 
 })(module.exports);
