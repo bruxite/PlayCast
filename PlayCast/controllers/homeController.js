6d2fec06b07885707a9f5d64cb98e37775b6af77
Mike Bruxvoort <mbruxvoort@bethany.org>
2015-08-21 22:03:01 -0400

    Initial Commit

---
+++ PlayCast/controllers/homeController.js
@@ -0,0 +1,14 @@
+﻿(function(homeController) {
+    homeController.init = function(app) {
+
+        var data = require("../data");
+
+        //Home Page
+        app.get("/", function (req, res) {
+            data.getGames(function(err, games) {
+                res.render("index", { title: "Home | PlayCast", error: err, games: games});
+            });
+        });
+    };
+
+})(module.exports);
